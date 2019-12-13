93e9140de73b20402cdcd2e4145af2516ef441c9
shengangxu <13545399131@163.com>
2019-12-13 17:14:15 +0800

    first commit

---
+++ pages/info/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false
+  },
+  
+  onLoad: function () {
+    // console.log(app)
+    // console.log(app.globalData)
+    // this.setData({
+    //   userInfo: app.globalData.userInfo,
+    //   hasUserInfo: true
+    // })
+    // console.log(this.userInfo)
+    // console.log(this.hasUserInfo)
+  },
+  onReady:function () {
+    
+  }
+})
